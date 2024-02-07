ea387511ea83686fe06462920c5b4b2b3ea51b32
fribiro <festusribiro@gmail.com>
2024-02-07 19:32:49 +0300

    Comment update

--- inputfunc.py
+++ inputfunc.py
@@ -7,7 +7,7 @@ sum_result = add_numbers(5,3)
 # print(sum_result)
 
 #docstring functions
-#used to document functioncs, classes or modules
+#used to document functions, classes or modules
 def my_function(parameters):
     '''
     this is a docstring
@@ -23,12 +23,13 @@ def allowed_to_drive(age):
 # print(allowed_to_drive(42))
 # print(allowed_to_drive(12))
 
+# operator and or 
 def is_laundry_day(today, laundry_day = "Monday", on_vacation = False):
     if  today is laundry_day and today is not "Sunday" and on_vacation is False:
         return True
     else:
         return False
     
-print(is_laundry_day("Monday"))
-print(is_laundry_day("Wednesday", "Tuesday"))
-print(is_laundry_day("Tuesday", "Tuesday", True))
+#print(is_laundry_day("Monday"))
+# print(is_laundry_day("Monday", "Thursday"))
+# print(is_laundry_day("Tuesday", "Monday", False))
