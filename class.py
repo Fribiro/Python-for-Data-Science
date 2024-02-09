b67c9aee826fd9a13ce8f30ba16fc8ead8728328
fribiro <festusribiro@gmail.com>
2024-02-09 19:54:58 +0300

    Multilevel inheritance

--- class.py
+++ class.py
@@ -92,3 +92,93 @@ def make_drive(vehicle):
 
 truck = Truck()
 print(make_drive(truck))
+
+#single inheritance
+class Animal:
+    def __init__(self,carn):
+        self.carn = carn
+
+    def speak(self):
+        print("Animal Speaking")
+
+    def food(self):
+        return f"I am a {self.carn}"
+
+class Lion(Animal):
+    def __init__(self,carn):
+        super().__init__(carn)
+    def roar(Self):
+        print("Lion roaring")
+
+d = Lion("Carnivore")
+# d.roar()
+# d.speak()
+print(d.food())
+
+class Giraffe(Animal):
+    def __init__(self,carn,herb):
+        super().__init__(carn)
+        self.herb = herb
+    def herbivore(self):
+        return f"I am a {self.herb}"
+
+g = Giraffe("Not","Herbivore")
+print(g.herbivore())
+
+#multiple inheritance
+class Calculation1:
+    def Addition(self,x,y):
+        return x+y
+class Calculation2:
+    def Multiplication(self,x,y):
+        return x*y
+    
+class Derived(Calculation1,Calculation2):
+    def Division(self,a,b):
+        return a/b
+
+d = Derived()
+print(d.Addition(2,4)) #6
+print(d.Multiplication(2,4)) #8
+print(d.Division(2,4)) #0.5
+
+class Animal:
+    def __init__(self,name,weight,age):
+        self.name = name
+        self.weight = weight
+        self.age = age
+
+    def speak(self):
+        print("Animal Speaking")
+
+    def animal_weight(self):
+        return f"{self.name} weighs {self.weight}"
+    
+    def animal_age(self):
+        return f"{self.name} is {self.age} months old"
+
+#The child class Lion inherits the base class Animal
+class Lion(Animal):
+    def __init__(self,name,weight,age,jaw):
+        super().__init__(name,weight,age)
+        self.jaw = jaw
+    
+    def roar(self):
+        print("Lion roaring")
+
+    def jaws(self):
+        print(f"I have a {self.jaw} jaw")
+
+#The child class BabyLion inherits another child class Lion
+class BabyLion(Lion):
+    def __init__(self,name,weight,age,jaw):
+        super().__init__(name,weight,age,jaw)
+    def eat(self):
+        print("Eating meat...")
+
+d = BabyLion("Cab",21, 4,"strong")
+# d.roar()
+# d.speak()
+# d.eat()
+print(d.animal_age())
+print(d.animal_weight())
